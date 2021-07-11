55b97d8e71271d46d4e3ad30588a0402d6fbb986
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-07-11 18:56:31 +0900

    models on dblayer

--- backend/src/models/models.go
+++ backend/src/models/models.go
@@ -1,31 +1,52 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 // 데이터베이스 레이어
 
 type Product struct {
+	gorm.Model
 	Image       string  `json:"img"`
-	ImageAlt    string  `json:"img_alt"`
+	ImageAlt    string  `gorm:"img_alt" json:"img_alt"`
 	Price       float64 `json:"price"`
-	Promotion   float64 `json:"promotion"`
-	ProductName string  `json:"product_name"`
-	Description string  `json:"description"`
+	Promotion   float64 `gorm:"promotion" json:"promotion"`
+	ProductName string  `gorm:"product_name" json:"product_name"`
+	Description string
+}
+
+func (Product) TableName() string {
+	return "products"
 }
 
 type Customer struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	LoggedIn  bool   `json:"loggedin"`
+	gorm.Model
+	Name      string  `json:"name"`
+	FirstName string  `gorm:"column:first_name" json:"first_name"`
+	LastName  string  `gorm:"column:last_name" json:"last_name"`
+	Email     string  `gorm:"column:email" json:"email"`
+	Password  string  `gorm:"column:password" json:"password"`
+	LoggedIn  bool    `gorm:"column:loggedin" json:"loggedin"`
+	Orders    []Order `json:"orders"`
+}
+
+func (Customer) TableName() string {
+	return "customers"
 }
 
 type Order struct {
+	gorm.Model
 	Product
 	Customer
-	CustomerID   int       `json:"customer_id"`
-	ProductID    int       `json:"product_id"`
-	Price        float64   `json:"price"`
-	PurchaseDate time.Time `json:"purchase_date"`
+	CustomerID   int       `gorm:"column:customer_id"`
+	ProductID    int       `gorm:"column:product_id"`
+	Price        float64   `gorm:"column:price" json:"price"`
+	PurchaseDate time.Time `gorm:"column:purchase_date" json:"purchase_date"`
+}
+
+func (Order) TableName() string {
+	return "orders"
 }
